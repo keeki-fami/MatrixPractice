284a3643efd93defb5716809747989e51a6a374e
TowaSakurada <towatowa3939@icloud.com>
2026-06-23 00:37:05 +0900

    Initial Commit

---
+++ LinearPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinearPractice
+//
+//  Created by 櫻田聖和 on 2026/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinearPractice/LinearPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearPracticeApp.swift
+//  LinearPractice
+//
+//  Created by 櫻田聖和 on 2026/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LinearPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
